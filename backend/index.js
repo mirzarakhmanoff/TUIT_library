7b5538530455854751d4ba1e4ebf546bf6178ebe
Ulugbek <mirzarakhmanoff@gmail.com>
2024-11-19 21:02:55 +0500

    add some changes

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3003" }));
+app.use(cors({ origin: "http://localhost:3002" }));
 app.use(express.json());
 
 // Эндпоинт для получения всех авторов
